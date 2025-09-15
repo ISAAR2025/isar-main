939550a7d5792943850bc3f0f2590abaa8b9f6e3
ISAAR2025 <it001@isaar.in>
2025-09-15 09:38:13 +0530

    Update Course.js

--- isar-backend/models/Course.js
+++ isar-backend/models/Course.js
@@ -2,9 +2,11 @@
 const mongoose = require('mongoose');
 
 const courseSchema = new mongoose.Schema({
-  course_name: String,
-  price: Number,
-  description: String
-}, { timestamps: true });
+  title : { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: String, required: true }, // keep string if you want "0.00"
+  image_url: { type: String, required: true },
+  created_at: { type: Date, default: Date.now }
+});
 
 module.exports = mongoose.model('Course', courseSchema);
