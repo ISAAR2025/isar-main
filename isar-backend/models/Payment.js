7b3b3d35a1c7434529882a0f34dbf70f5ec503f2
ISAAR2025 <it001@isaar.in>
2025-09-15 09:39:19 +0530

    Update Payment.js

--- isar-backend/models/Payment.js
+++ isar-backend/models/Payment.js
@@ -1,9 +1,8 @@
-// models/Payment.js
 const mongoose = require('mongoose');
 
 const PaymentSchema = new mongoose.Schema({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  course_name: { type: String, required: true },
+  title: { type: String, required: true },  // Course title
   razorpay_order_id: String,
   razorpay_payment_id: String,
   razorpay_signature: String,
